f3bb509dce092bf8ee24234edd46cdd0404b42d7
Siddharth Gond <siddharth.g@availfinance.in>
2020-02-01 21:15:27 +0530

    first commit

---
+++ src/graph/Graph.java
@@ -0,0 +1,7 @@
+package graph;
+
+public class Graph {
+    public static void main(String[] args) {
+        System.out.println("Hey, There");
+    }
+}
---
+++ src/queue/Queue.java
@@ -0,0 +1,7 @@
+package queue;
+
+public class Queue {
+    public static void main(String[] args) {
+        System.out.println("Hey, This is Queue");
+    }
+}
